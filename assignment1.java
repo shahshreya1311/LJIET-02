308202f5565b402083bc0e8d856335312588c9df
shahshreya1311 <98080246+shahshreya1311@users.noreply.github.com>
2023-05-29 19:53:50 +0530

    Add files via upload

---
+++ assignment1.java
@@ -0,0 +1,46 @@
+package trainingAssignments;
+
+	import java.util.Scanner;
+
+	class Circle {
+	    final double PI = 3.14159; // Constant value for PI
+	    double radius;
+	    double area;
+	    double circumference;
+
+	    // Method to accept radius from the user
+	    public void acceptRadius() {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter the radius of the circle: ");
+	        radius = scanner.nextDouble();
+	        scanner.close();
+	    }
+
+	    // Method to calculate the area of the circle
+	    public void calculateArea() {
+	        area = PI * radius * radius;
+	    }
+
+	    // Method to calculate the circumference of the circle
+	    public void calculateCircumference() {
+	        circumference = 2 * PI * radius;
+	    }
+
+	    // Method to display the area and circumference of the circle
+	    public void displayResult() {
+	        System.out.println("Radius: " + radius);
+	        System.out.println("Area: " + area);
+	        System.out.println("Circumference: " + circumference);
+	    }
+	}
+
+	public class assignment1 {
+	    public static void main(String[] args) {
+	        Circle circle = new Circle();
+	        circle.acceptRadius();
+	        circle.calculateArea();
+	        circle.calculateCircumference();
+	        circle.displayResult();
+	    }
+	}
+
